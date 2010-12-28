6f360b7c5e5674be49ec7cdcbd023739709ac903
Tomaž Muraus <kami@k5-storitve.net>
2010-12-28 23:16:51 +0100

    Update example.                 #ddconsole.log(data)

--- example/example.js
+++ example/example.js
@@ -1,5 +1,7 @@
 var sys = require('sys');
 
+var sprintf = require('../lib/extern/sprintf').sprintf;
+
 var Yubico = require('../index').Yubico;
 
 var argv = process.argv;
@@ -8,20 +10,37 @@ if (argv[0].lastIndexOf('node') === argv[0].length - 4) {
     argv = argv.slice(2);
 }
 else {
-    argv = argv.slice(2);
+    argv = argv.slice(1);
 }
 
-if (argv.length !== 3) {
-    sys.puts('Usage: node example.js client_id [secret_key] otp');
+var mode = (argv[0] === 'single') ? 'single' : 'multi';
+var expected_args = (mode === 'single') ? 3 : 4;
+
+var func, args;
+argv = argv.slice(1);
+
+if (argv.length !== expected_args) {
+    args = (mode === 'single') ? 'client_id [secret_key] otp' :
+                                        'client_id [secret_key] otp_1 otp_2';
+    sys.puts(sprintf('Usage: node example.js %s', args));
     process.exit(1);
 }
 
 var client_id = argv[0];
 var secret_key = (argv[1] === 'null' || argv[1] === 'none') ? null : argv[1];
-var otp = argv[2];
 
 var yubico = new Yubico(client_id, secret_key);
-yubico.verify(otp, function(err, success) {
+
+if (mode === 'single') {
+    func = yubico.verify;
+    args = argv[2];
+}
+else if (mode === 'multi') {
+    func = yubico.verify_multi;
+    args = argv.slice(2);
+}
+
+func.call(yubico, args, function(err, success) {
     if (err) {
         sys.print('Token validation failed: ' + err.message);
         return;
